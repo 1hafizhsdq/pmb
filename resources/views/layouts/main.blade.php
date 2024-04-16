082d7d4fd9d1f3ef975a259b89aaf699f9c7df71
Hafizh Assidiq <hafizhassidiq77@gmail.com>
2024-04-16 20:34:21 +0700

    perbaikan template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
 
 <body>
     <div class="container-scroller">
-        <div class="row p-0 m-0 proBanner" id="proBanner">
+        <div class="row p-0 m-0 proBanner" id="proBanner" style="z-index:-1000;">
             <div class="col-md-12 p-0 m-0">
                 <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
                     <div class="ps-lg-1">
