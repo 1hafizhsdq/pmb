a3c79f3234a0b413490d6cda3c6d710522f451ce
hafizhsdq <hafizhassidiq77@gmail.com>
2024-05-10 09:24:24 +0700

    update asdasda

---
+++ public/select2/src/js/select2/translationa.js
@@ -0,0 +1,14 @@
+define([
+  'jquery',
+  'require'
+], function ($, require) {
+  function Translation (dict) {
+    this.dict = dict || {};
+  }
+
+  Translation.prototype.all = function () {
+    return this.dict;
+  };
+
+  return Translation;
+});
