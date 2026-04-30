9d387bb31ea38da62af094bf277309210d0dd69d
1hafizhsdq <hafizhassidiq77@gmail.com>
2026-04-30 17:46:30 +0700

    update enabled pmb

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -78,6 +78,7 @@ class LoginController extends Controller
     {
         $tanggal_saat_ini = Carbon::now();
         $data['periode'] = Periode::with('pmb')
+                            ->where('is_active', 1)
                             ->whereHas('pmb', function ($query) {
                                 $query->whereDate('tgl_awal_pmb', '<=', Carbon::today())
                                     ->whereDate('tgl_akhir_pmb', '>=', Carbon::today());
